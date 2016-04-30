1e21f71db9303f9b10bbd4a916e19093e8a526bf
PeterDamrosch <peter.damrosch@gmail.com>
2016-04-30 00:05:16 +0000

    Updated git ignore and config

---
+++ hoods/config.py
@@ -0,0 +1,8 @@
+class DevelopmentConfig(object):
+    TABLE_POINTS = "neighborhoodpoints"
+    DEBUG = True
+
+class TestingConfig(object):
+    TABLE_POINTS = "neighborhoodspointstest"
+    DEBUG = True
+    
