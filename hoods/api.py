c9419aabdbdab39a9cd28aaf9d8d4aa5cea13f5c
PeterDamrosch <peter.damrosch@gmail.com>
2016-04-30 00:02:43 +0000

    Initial commit

---
+++ hoods/api.py
@@ -0,0 +1,67 @@
+import os
+import os.path
+import json
+import requests
+
+from flask import request, Response, url_for
+from jsonschema import validate, ValidationError
+
+from . import decorators
+from hoods import app
+
+# Get table name from config - different tables for testing and dev
+table_points = app.config["TABLE_POINTS"]
+cartodb_key = os.environ["CARTODB_KEY"]
+
+# JSON Schema describing the structure of the POST request data
+point_schema = {
+    "properties": {
+        "coordinates" : {
+            "type": "object",
+            "properties": {"lat" : {"type": "string"},
+                            "lng" : {"type": "string"}
+            },
+        "required": ["lat","lng"]
+        },
+        "name": {"type": "string"},
+        "description": {"type":"string"}
+    },
+    "required": ["coordinates","name"]
+}
+
+@app.route("/api/geometry", methods=["POST"])
+@decorators.require("application/json")
+def point_post():
+    """ Post a point from client map to CartoDB table """
+    data = request.json
+    
+    # Check that JSON is in right format using the schema, if not return 422
+    try:
+        validate(data,point_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+    
+    # Get data from request
+    coordinates = data["coordinates"]
+    enteredDescription = data["description"]
+    enteredName = data["name"]
+    
+    # Compose SQL Query
+    sql1="INSERT INTO "+table_points+" (the_geom, description, name) VALUES (ST_SetSRID(ST_GeomFromGeoJSON('"
+    sql2='{"type":"Point","coordinates":[' +coordinates['lng'] + "," + coordinates['lat'] + "]}'),4326),'" + enteredDescription +"','" +enteredName +"')"
+    sql_query = sql1 + sql2
+    
+    # Parameters dictionary for requests with query and api key
+    params = {"q": sql_query,"api_key": cartodb_key}
+    
+    # Send insert query to CartoDB
+    r = requests.post("https://peterdamrosch.cartodb.com/api/v2/sql",params=params)
+    
+    # Check that POST was successful, send client success message
+    if r.json()['total_rows'] == 1:
+        data_response =  {"result": "Feature successfully added to CartoDB"}
+        return Response(json.dumps(data_response), 201, mimetype="application/json")
+
+
+    
---
+++ tests/api_tests.py
@@ -0,0 +1,65 @@
+import unittest
+import os
+import shutil
+import json
+try: from urllib.parse import urlparse
+except ImportError: from urlparse import urlparse # Py2 compatibility
+from io import StringIO,BytesIO
+
+import sys; print(list(sys.modules.keys()))
+
+# Configure our app to use the testing databse
+os.environ["CONFIG_PATH"] = "hoods.config.TestingConfig"
+
+from hoods import app
+
+class TestAPI(unittest.TestCase):
+    """ Tests for the hoods API """
+
+    def setUp(self):
+        """ Test setup """
+        self.client = app.test_client()
+
+    def test_post_point(self):
+        a = {"lat": "31.954014","lng": "35.910794"}
+        enteredName = "Somewhere else in Amman"
+        enteredDescription = "Test works inshallah"
+       
+        data = {
+            "coordinates": a,
+            "description": enteredDescription,
+            "name": enteredName
+        }
+        print(data)
+        
+        response = self.client.post("/api/geometry",
+            data=json.dumps(data),
+            content_type="application/json",
+            headers=[("Accept", "application/json")]
+        )
+        
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.mimetype, "application/json")
+    
+    def test_post_point_schema_wrong(self):
+        #Wrong form for point coordinate, should be dictionary/json object
+        a = {"lat": 31.954014,"lng": 35.910794}
+        enteredName = "Jabal Hussein Test"
+        enteredDescription = "Test Input of Jabal Hussein"
+       
+        data = {
+            "coordinates": a,
+            "description": enteredDescription,
+            "name": enteredName
+        }
+        print(data)
+        
+        response = self.client.post("/api/geometry",
+            data=json.dumps(data),
+            content_type="application/json",
+            headers=[("Accept", "application/json")]
+        )
+        print(response.status_code)
+        
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(response.mimetype, "application/json")
